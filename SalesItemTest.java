cb0321614914a67f0ec779aec3829bc2eb5ee596
coreyllcooper <90771688+coreyllcooper@users.noreply.github.com>
2021-11-08 14:28:22 -0330

    Update SalesItemTest.java
    Added my code

--- SalesItemTest.java
+++ SalesItemTest.java
@@ -48,5 +48,41 @@ public class SalesItemTest
         assertEquals("Bike", salesIte1.getName());
         assertEquals(0, salesIte1.getNumberOfComments());
     }
+    
+    @Test
+    public void getRatingTest()
+    {
+        SalesItem salesIte2 = new SalesItem("Bike", 100);
+        Comment comment2 = new Comment("Corey", "This bike is bunk.", 2);
+        assertEquals(2, comment2.getRating());
+    }
+
+    @Test
+    public void ratingsInvalidTest()
+    {
+        SalesItem salesIte1 = new SalesItem("Bike", 100);
+        Comment comment1 = new Comment("Corey", "This is the best bike ever.", 6);
+        assertEquals(true, salesIte1.ratingInvalid(6));
+        assertEquals(true, salesIte1.ratingInvalid(-1));
+        assertEquals(false, salesIte1.ratingInvalid(1));
+    }
+
+
+    @Test
+    public void findCommentByAuthorTest()
+    {
+        SalesItem salesIte2 = new SalesItem("Bike", 100);
+        Comment comment2 = new Comment("test", "review", 5);
+        assertEquals("test", salesIte2.findCommentByAuthor("test"));
+    }
+
+    @Test
+    public void findMostHelpfulCommentTest()
+    {
+        SalesItem salesIte1 = new SalesItem("Bike", 100);
+        Comment comment1 = new Comment("Corey", "This bike bikes", 2);
+        Comment comment2 = new Comment("Bob", "This bike bikes poorly", 1);
+        comment1.upvote();
+    }
 }
 
